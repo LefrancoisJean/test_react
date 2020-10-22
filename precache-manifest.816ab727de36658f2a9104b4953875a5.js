519478b320139aca1e92f808121d37934f45c406
jlefrancois <jean.lefrancois@docapost.fr>
2020-10-22 16:45:22 +0200

    Updates

---
+++ precache-manifest.816ab727de36658f2a9104b4953875a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1622b01d7440bea195610455dc9055c3",
+    "url": "/test_react/index.html"
+  },
+  {
+    "revision": "28e9757fe529e78f4eaa",
+    "url": "/test_react/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "4c4d4df64c222a1d1b2e",
+    "url": "/test_react/static/css/main.681e1c8d.chunk.css"
+  },
+  {
+    "revision": "28e9757fe529e78f4eaa",
+    "url": "/test_react/static/js/2.6bd7064a.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/test_react/static/js/2.6bd7064a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4d4df64c222a1d1b2e",
+    "url": "/test_react/static/js/main.e63ff109.chunk.js"
+  },
+  {
+    "revision": "5f851837011c24b7ff1f",
+    "url": "/test_react/static/js/runtime-main.227f5ba9.js"
+  }
+]);
